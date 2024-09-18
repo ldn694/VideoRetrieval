e8cf27d89d70627695e7ee34e9297d80d39bddaa
BJMinhNhut <nhutben678@gmail.com>
2024-09-18 15:50:41 +0700

    No longer need to type data path

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 DATA_PATH = 'D:/AIC24/Data'
 
 try:
-    from .local_settings import *
+    from local_settings import *
 except ImportError:
     pass
--- web.py
+++ web.py
@@ -11,6 +11,7 @@ import json
 import csv
 from googletrans import Translator
 from VideoRetrieval import translate_query, retrieve_frames, convert_to_suggestion_input, create_suggestion
+import settings
 
 app = Flask(__name__)
 app.secret_key = 'Yeu Phuong Anh<3'  # Necessary for session
@@ -22,29 +23,34 @@ max_cache_query_size = 10
 max_cache_frame_size = 1000
 
 # Function to translate the query
+
+
 def translate_query(query):
     translator = Translator()
     translated_query = translator.translate(query, dest='en').text
     return translated_query
 
+
 # Global model variables (loaded once)
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, _, preprocess = open_clip.create_model_and_transforms('MobileCLIP-B', pretrained='datacompdr_lt', device=device)
+model, _, preprocess = open_clip.create_model_and_transforms(
+    'MobileCLIP-B', pretrained='datacompdr_lt', device=device)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Get input values
         query = request.form['query']
-        folder_path = request.form['folder_path']
+        folder_path = settings.DATA_PATH
         num_frames = int(request.form['num_frames'])
         view_mode = request.form['view_mode']
 
         if view_mode == 'frame':
-            top_frames, list_frames = retrieve_frames(query, folder_path, num_frames)
+            top_frames, list_frames = retrieve_frames(
+                query, folder_path, num_frames)
             # Generate a unique key for this query
             cache_query_key = f"{query}_{folder_path}_{num_frames}"
-
 
             # Check if the frames are already cached in memory
             if cache_query_key not in cache_query:
@@ -53,7 +59,8 @@ def index():
                     oldest_key = next(iter(cache_query))
                     del cache_query[oldest_key]
                 # Retrieve frames if they are not cached
-                top_frames, list_frames = retrieve_frames(query, folder_path, num_frames)
+                top_frames, list_frames = retrieve_frames(
+                    query, folder_path, num_frames)
                 # Store the frames in the global cache
                 cache_query[cache_query_key] = (top_frames, list_frames)
 
@@ -75,15 +82,18 @@ def index():
                         oldest_key = next(iter(cache_frame))
                         del cache_frame[oldest_key]
                     # Load the image
-                    img = Image.open(os.path.join(folder_path, 'keyframes', video_name, file_name))
+                    img = Image.open(os.path.join(
+                        folder_path, 'keyframes', video_name, file_name))
                     # Convert image to base64
                     buffered = io.BytesIO()
                     img.save(buffered, format="JPEG")
-                    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+                    img_str = base64.b64encode(
+                        buffered.getvalue()).decode("utf-8")
                     cache_frame[cache_frame_key] = img_str
                 else:
                     img_str = cache_frame[cache_frame_key]
-                images.append((img_str, video_name, frame_idx, sim, minute, second))
+                images.append(
+                    (img_str, video_name, frame_idx, sim, minute, second))
 
             return render_template('index.html', images=images, query=query, folder_path=folder_path, num_frames=num_frames)
 
@@ -92,13 +102,15 @@ def index():
             queries = query.split(';')
             suggestions_inputs = []
             for query in queries:
-                top_frames, list_frames = retrieve_frames(query, folder_path, num_frames)
-                suggestions_input = convert_to_suggestion_input(top_frames, list_frames)
+                top_frames, list_frames = retrieve_frames(
+                    query, folder_path, num_frames)
+                suggestions_input = convert_to_suggestion_input(
+                    top_frames, list_frames)
                 suggestions_inputs.append(suggestions_input)
 
             # Create suggestions for clips
             suggestions = create_suggestion(suggestions_inputs)
-            
+
             # Load the video_id.txt file to map video names to YouTube URLs
             video_urls = {}
             with open('video_id.txt', 'r') as f:
@@ -115,9 +127,12 @@ def index():
     return render_template('index.html')
 
 # New route to handle the extraction of the current order and save to CSV
+
+
 @app.route('/download_csv', methods=['POST'])
 def download_csv():
-    data = json.loads(request.form['order'])  # Receive the order from the frontend
+    # Receive the order from the frontend
+    data = json.loads(request.form['order'])
     custom_text = request.form['custom_text']  # Receive custom text
 
     # Create a CSV file
@@ -129,12 +144,14 @@ def download_csv():
         for row in data:
             # If custom text is provided, add it as an extra column
             if custom_text:
-                writer.writerow([row['video_name'], row['frame_idx'], custom_text])
+                writer.writerow(
+                    [row['video_name'], row['frame_idx'], custom_text])
             else:
                 writer.writerow([row['video_name'], row['frame_idx']])
 
     # Send the file to the client using 'download_name' instead of 'attachment_filename'
     return send_file(csv_filepath, as_attachment=True, download_name=csv_filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
